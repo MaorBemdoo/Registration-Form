8f139ed970d863f921fbb54b379bf2ed8955e34e
Bemdoo Maor <bemdoo.maor1@gmail.com>
2023-11-04 23:11:16 +0100

    Made form controlled

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Button from "@mui/material/Button"
 import OutlinedInput from "@mui/material/OutlinedInput"
 import FormHelperText from "@mui/material/FormHelperText"
 import {GlobalStyles} from "./GlobalStyles"
+import { useState } from "react"
 
 function App() {
 
@@ -22,32 +23,34 @@ function App() {
     height: "400px"
   }
 
+  const [user, setUser] = useState(null)
+
   return (
     <Card component="form" elevation={3} variant="outlined" sx={CardStyle}>
       <GlobalStyles/>
       <Card variant="outlined" sx={{backgroundColor: "#d41d1d", padding: "1em", width: "calc(400px - 2em)"}} hidden>All fields are required</Card>
       <FormControl variant="outlined" color="success" fullWidth>
         <InputLabel htmlFor="firstname">First Name</InputLabel>
-        <OutlinedInput id="firstname" label="First Name" aria-describedby="firstname-text"/>
+        <OutlinedInput id="firstname" label="First Name" value={user?.firstname} onChange={(e) => setUser({...user,firstname: e.target.value})} aria-describedby="firstname-text"/>
         <FormHelperText id="firstname-text" hidden>First name is required</FormHelperText>
       </FormControl>
       <FormControl variant="outlined" color="success" fullWidth>
         <InputLabel htmlFor="lastname">Last Name</InputLabel>
-        <OutlinedInput id="lastname" label="Last Name" aria-describedby="lastname-text"/>
+        <OutlinedInput id="lastname" label="Last Name" value={user?.lastname} onChange={(e) => setUser({...user,lastname: e.target.value})} aria-describedby="lastname-text"/>
         <FormHelperText id="lastname-text" hidden>Last name is required</FormHelperText>
       </FormControl>
       <FormControl variant="outlined" color="success" fullWidth>
         <InputLabel htmlFor="othername">Other Names</InputLabel>
-        <OutlinedInput id="othername" label="Other Names" aria-describedby="othername-text"/>
+        <OutlinedInput id="othername" label="Other Names" value={user?.othername} onChange={(e) => setUser({...user,othername: e.target.value})} aria-describedby="othername-text"/>
       </FormControl>
       <FormControl variant="outlined"color="success" fullWidth>
         <InputLabel htmlFor="phonenum">Phone Number</InputLabel>
-        <OutlinedInput id="phonenum" label="Phone Number" aria-describedby="phonenum-text"/>
+        <OutlinedInput id="phonenum" label="Phone Number" value={user?.phonenum} onChange={(e) => setUser({...user,phonenum: e.target.value})} aria-describedby="phonenum-text"/>
         <FormHelperText id="phonenum-text" hidden>Phone number is invalid</FormHelperText>
       </FormControl>
       <FormControl variant="outlined" color="success" fullWidth>
         <InputLabel htmlFor="regnum">Registration Number</InputLabel>
-        <OutlinedInput id="regnum" label="Registration Number" aria-aria-describedby="regnum-text"/>
+        <OutlinedInput id="regnum" label="Registration Number" value={user?.regnum} onChange={(e) => setUser({...user,regnum: e.target.value})} aria-describedby="regnum-text"/>
         <FormHelperText id="regnum-text" hidden>Registration number is required</FormHelperText>
       </FormControl>
       <Button variant="contained" color="success">Submit</Button>
