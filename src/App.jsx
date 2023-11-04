7e670f87b355c36364ddde5062db7c690d2ff627
Bemdoo Maor <bemdoo.maor1@gmail.com>
2023-11-04 22:46:41 +0100

    Added error text

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import InputLabel from "@mui/material/InputLabel"
 import Card from "@mui/material/Card"
 import Button from "@mui/material/Button"
 import OutlinedInput from "@mui/material/OutlinedInput"
+import FormHelperText from "@mui/material/FormHelperText"
 import {GlobalStyles} from "./GlobalStyles"
 
 function App() {
@@ -26,23 +27,27 @@ function App() {
       <GlobalStyles/>
       <FormControl variant="outlined" color="success" fullWidth>
         <InputLabel htmlFor="firstname">First Name</InputLabel>
-        <OutlinedInput id="firstname" label="First Name"/>
+        <OutlinedInput id="firstname" label="First Name" aria-describedby="firstname-text"/>
+        <FormHelperText id="firstname-text" hidden>First name is required</FormHelperText>
       </FormControl>
       <FormControl variant="outlined" color="success" fullWidth>
         <InputLabel htmlFor="lastname">Last Name</InputLabel>
-        <OutlinedInput id="lastname" label="Last Name"/>
+        <OutlinedInput id="lastname" label="Last Name" aria-describedby="lastname-text"/>
+        <FormHelperText id="lastname-text" hidden>Last name is required</FormHelperText>
       </FormControl>
       <FormControl variant="outlined" color="success" fullWidth>
         <InputLabel htmlFor="othername">Other Names</InputLabel>
-        <OutlinedInput id="othername" label="Other Names"/>
+        <OutlinedInput id="othername" label="Other Names" aria-describedby="othername-text"/>
       </FormControl>
       <FormControl variant="outlined"color="success" fullWidth>
         <InputLabel htmlFor="phonenum">Phone Number</InputLabel>
-        <OutlinedInput id="phonenum" label="Phone Number"/>
+        <OutlinedInput id="phonenum" label="Phone Number" aria-describedby="phonenum-text"/>
+        <FormHelperText id="phonenum-text" hidden>Phone number is invalid</FormHelperText>
       </FormControl>
       <FormControl variant="outlined" color="success" fullWidth>
         <InputLabel htmlFor="regnum">Registration Number</InputLabel>
-        <OutlinedInput id="regnum" label="Registration Number"/>
+        <OutlinedInput id="regnum" label="Registration Number" aria-aria-describedby="regnum-text"/>
+        <FormHelperText id="regnum-text" hidden>Registration number is required</FormHelperText>
       </FormControl>
       <Button variant="contained" color="success">Submit</Button>
     </Card>
