c4cee9eb9dd1713856c4ed4d5920c554daf7dd06
Bemdoo Maor <bemdoo.maor1@gmail.com>
2023-11-04 21:58:22 +0100

    Worked on markup

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,34 @@
-import TextField from "@mui/material/TextField"
+import FormControl from "@mui/material/FormControl"
+import InputLabel from "@mui/material/InputLabel"
 import Card from "@mui/material/Card"
 import Button from "@mui/material/Button"
+import OutlinedInput from "@mui/material/OutlinedInput"
 
 function App() {
 
   return (
     <Card component="form" elevation={3} variant="outlined">
-      <TextField></TextField>
+      <FormControl variant="outlined">
+        <InputLabel htmlFor="firstname">First Name</InputLabel>
+        <OutlinedInput id="firstname" label="First Name"/>
+      </FormControl>
+      <FormControl variant="outlined">
+        <InputLabel htmlFor="lastname">Last Name</InputLabel>
+        <OutlinedInput id="lastname" label="Last Name"/>
+      </FormControl>
+      <FormControl variant="outlined">
+        <InputLabel htmlFor="othername">Other Names</InputLabel>
+        <OutlinedInput id="othername" label="Other Names"/>
+      </FormControl>
+      <FormControl variant="outlined">
+        <InputLabel htmlFor="phonenum">Phone Number</InputLabel>
+        <OutlinedInput id="phonenum" label="Phone Number"/>
+      </FormControl>
+      <FormControl variant="outlined">
+        <InputLabel htmlFor="regnum">Registration Number</InputLabel>
+        <OutlinedInput id="regnum" label="Registration Number"/>
+      </FormControl>
+      <Button variant="contained" color="success">Submit</Button>
     </Card>
   )
 }
