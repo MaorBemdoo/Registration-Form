ef5945f82520a3a3afd91b764f5812d9a0647b3f
Bemdoo Maor <bemdoo.maor1@gmail.com>
2023-11-04 21:21:18 +0100

    Added Card

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
+import Card from "@mui/material/Card"
 
 function App() {
 
   return (
-    <>
-      
-    </>
+    <Card component="form" elevation={3} variant="outlined">
+      Heyo
+    </Card>
   )
 }
 
