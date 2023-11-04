a82c38079424be13e5d873a2427103e6caecb3a3
Bemdoo Maor <bemdoo.maor1@gmail.com>
2023-11-04 23:24:04 +0100

    Added an error state

--- src/App.jsx
+++ src/App.jsx
@@ -24,20 +24,25 @@ function App() {
   }
 
   const [user, setUser] = useState(null)
+  const [error, setError] = useState(false)
+
+  const submitHandler = (e) => {
+    
+  }
 
   return (
-    <Card component="form" elevation={3} variant="outlined" sx={CardStyle}>
+    <Card component="form" variant="outlined" sx={CardStyle}>
       <GlobalStyles/>
-      <Card variant="outlined" sx={{backgroundColor: "#d41d1d", padding: "1em", width: "calc(400px - 2em)"}} hidden>All fields are required</Card>
+      <Card variant="outlined" sx={{backgroundColor: "#d41d1d", padding: "1em", width: "calc(400px - 2em)"}} hidden={!error?.universal}>All fields are required</Card>
       <FormControl variant="outlined" color="success" fullWidth>
         <InputLabel htmlFor="firstname">First Name</InputLabel>
         <OutlinedInput id="firstname" label="First Name" value={user?.firstname} onChange={(e) => setUser({...user,firstname: e.target.value})} aria-describedby="firstname-text"/>
-        <FormHelperText id="firstname-text" hidden>First name is required</FormHelperText>
+        <FormHelperText id="firstname-text" hidden={!error?.firstname}>First name is required</FormHelperText>
       </FormControl>
       <FormControl variant="outlined" color="success" fullWidth>
         <InputLabel htmlFor="lastname">Last Name</InputLabel>
         <OutlinedInput id="lastname" label="Last Name" value={user?.lastname} onChange={(e) => setUser({...user,lastname: e.target.value})} aria-describedby="lastname-text"/>
-        <FormHelperText id="lastname-text" hidden>Last name is required</FormHelperText>
+        <FormHelperText id="lastname-text" hidden={!error?.lastname}>Last name is required</FormHelperText>
       </FormControl>
       <FormControl variant="outlined" color="success" fullWidth>
         <InputLabel htmlFor="othername">Other Names</InputLabel>
@@ -46,14 +51,14 @@ function App() {
       <FormControl variant="outlined"color="success" fullWidth>
         <InputLabel htmlFor="phonenum">Phone Number</InputLabel>
         <OutlinedInput id="phonenum" label="Phone Number" value={user?.phonenum} onChange={(e) => setUser({...user,phonenum: e.target.value})} aria-describedby="phonenum-text"/>
-        <FormHelperText id="phonenum-text" hidden>Phone number is invalid</FormHelperText>
+        <FormHelperText id="phonenum-text" hidden={!error?.phonenum}>Phone number is invalid</FormHelperText>
       </FormControl>
       <FormControl variant="outlined" color="success" fullWidth>
         <InputLabel htmlFor="regnum">Registration Number</InputLabel>
         <OutlinedInput id="regnum" label="Registration Number" value={user?.regnum} onChange={(e) => setUser({...user,regnum: e.target.value})} aria-describedby="regnum-text"/>
-        <FormHelperText id="regnum-text" hidden>Registration number is required</FormHelperText>
+        <FormHelperText id="regnum-text" hidden={!error?.regnum}>Registration number is required</FormHelperText>
       </FormControl>
-      <Button variant="contained" color="success">Submit</Button>
+      <Button variant="contained" color="success" onClick={submitHandler}>Submit</Button>
     </Card>
   )
 }
