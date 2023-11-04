3ab62dedbbe3d9fdf01a69224aa1f3fde14596d0
Bemdoo Maor <bemdoo.maor1@gmail.com>
2023-11-04 23:40:17 +0100

    Worked on submitHn=andler

--- src/App.jsx
+++ src/App.jsx
@@ -23,40 +23,48 @@ function App() {
     height: "400px"
   }
 
-  const [user, setUser] = useState(null)
+  const [user, setUser] = useState({
+    firstname: "",
+    lastname: "",
+    othername: "",
+    phonenum: "",
+    regnum: ""
+  })
   const [error, setError] = useState(false)
 
-  const submitHandler = (e) => {
-    
+  const submitHandler = () => {
+    if(user?.firstname.trim() == 0){
+      setError({...error, firstname: true})
+    }
   }
 
   return (
     <Card component="form" variant="outlined" sx={CardStyle}>
       <GlobalStyles/>
-      <Card variant="outlined" sx={{backgroundColor: "#d41d1d", padding: "1em", width: "calc(400px - 2em)"}} hidden={!error?.universal}>All fields are required</Card>
-      <FormControl variant="outlined" color="success" fullWidth>
+      <Card variant="outlined" sx={{backgroundColor: "#d41d1d", padding: "1em", width: "calc(400px - 2em)"}} hidden={!(error?.universal)} error={error?.universal}>All fields are required</Card>
+      <FormControl variant="outlined" color="success" error={error?.firstname} fullWidth>
         <InputLabel htmlFor="firstname">First Name</InputLabel>
         <OutlinedInput id="firstname" label="First Name" value={user?.firstname} onChange={(e) => setUser({...user,firstname: e.target.value})} aria-describedby="firstname-text"/>
-        <FormHelperText id="firstname-text" hidden={!error?.firstname}>First name is required</FormHelperText>
+        <FormHelperText id="firstname-text" hidden={!(error?.firstname)}>First name is required</FormHelperText>
       </FormControl>
-      <FormControl variant="outlined" color="success" fullWidth>
+      <FormControl variant="outlined" color="success" error={error?.lastname} fullWidth>
         <InputLabel htmlFor="lastname">Last Name</InputLabel>
         <OutlinedInput id="lastname" label="Last Name" value={user?.lastname} onChange={(e) => setUser({...user,lastname: e.target.value})} aria-describedby="lastname-text"/>
-        <FormHelperText id="lastname-text" hidden={!error?.lastname}>Last name is required</FormHelperText>
+        <FormHelperText id="lastname-text" hidden={!(error?.lastname)}>Last name is required</FormHelperText>
       </FormControl>
       <FormControl variant="outlined" color="success" fullWidth>
         <InputLabel htmlFor="othername">Other Names</InputLabel>
         <OutlinedInput id="othername" label="Other Names" value={user?.othername} onChange={(e) => setUser({...user,othername: e.target.value})} aria-describedby="othername-text"/>
       </FormControl>
-      <FormControl variant="outlined"color="success" fullWidth>
+      <FormControl variant="outlined"color="success" error={error?.phonenum} fullWidth>
         <InputLabel htmlFor="phonenum">Phone Number</InputLabel>
         <OutlinedInput id="phonenum" label="Phone Number" value={user?.phonenum} onChange={(e) => setUser({...user,phonenum: e.target.value})} aria-describedby="phonenum-text"/>
-        <FormHelperText id="phonenum-text" hidden={!error?.phonenum}>Phone number is invalid</FormHelperText>
+        <FormHelperText id="phonenum-text" hidden={!(error?.phonenum)}>Phone number is invalid</FormHelperText>
       </FormControl>
-      <FormControl variant="outlined" color="success" fullWidth>
+      <FormControl variant="outlined" color="success" error={error?.regnum} fullWidth>
         <InputLabel htmlFor="regnum">Registration Number</InputLabel>
         <OutlinedInput id="regnum" label="Registration Number" value={user?.regnum} onChange={(e) => setUser({...user,regnum: e.target.value})} aria-describedby="regnum-text"/>
-        <FormHelperText id="regnum-text" hidden={!error?.regnum}>Registration number is required</FormHelperText>
+        <FormHelperText id="regnum-text" hidden={!(error?.regnum)}>Registration number is required</FormHelperText>
       </FormControl>
       <Button variant="contained" color="success" onClick={submitHandler}>Submit</Button>
     </Card>
