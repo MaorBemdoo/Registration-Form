4dc9894bd5a0313bd9eb9554d050b6558952fddc
Bemdoo Maor <bemdoo.maor1@gmail.com>
2023-11-04 21:25:42 +0100

    Imported TextField and used it with Button

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
+import TextField from "@mui/material/TextField"
 import Card from "@mui/material/Card"
+import Button from "@mui/material/Button"
 
 function App() {
 
   return (
     <Card component="form" elevation={3} variant="outlined">
-      Heyo
+      <TextField></TextField>
     </Card>
   )
 }
