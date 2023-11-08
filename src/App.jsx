d14a9ead9cc63975abd73979dea1a51d3d25b2a0
Bemdoo Maor <bemdoo.maor1@gmail.com>
2023-11-08 19:37:03 +0100

    Reworked on error handling

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import OutlinedInput from "@mui/material/OutlinedInput"
 import FormHelperText from "@mui/material/FormHelperText"
 import {GlobalStyles} from "./GlobalStyles"
 import { useState, useRef, useEffect } from "react"
+import FormLabel from '@mui/material/FormLabel'
 
 function App() {
 
@@ -30,23 +31,62 @@ function App() {
     phonenum: "",
     regnum: ""
   })
-  const [error, setError] = useState(false)
+  const [uniError, setUniError] = useState(false)
+  const [fnError, setFnError] = useState(false)
+  const [lnError, setLnError] = useState(false)
+  const [pnError, setPnError] = useState(false)
+  const [rnError, setRnError] = useState(false)
   const inputRefContainer = useRef(null)
 
   const phoneNumRegex = /^(0|\+\d{1,4})\d{10}$/g
   const regNumRegex = /^[A-Z]{3}\/\d{2}\/\d{2}\/\d{2}\/\d{4}$/g
 
-  const submitHandler = () => {
+  const validForm = () => {
+    let isValid = true;
+
+    // validate all fields
     if(user.firstname.trim() == "" && user.lastname.trim() == "" && user.phonenum.trim() == "" && user.regnum.trim() == ""){
-      setError({...error, universal: true})
-    } else if(user.firstname.trim() == ""){
-      setError({...error, firstname: true})
-    } else if(user.lastname.trim() == ""){
-      setError({...error, lastname: true})
-    } else if(user.phonenum.trim() == "" || !(phoneNumRegex.test(user.phonenum))){
-      setError({...error, phonenum: true})
-    } else if(user.regnum.trim() == "" || !(regNumRegex.test(user.regnum))){
-      setError({...error, regnum: true})
+      setUniError(true)
+      isValid = false;
+    } else{
+      setUniError(false)
+    }
+
+    // validate name
+    if(user.firstname.trim() == ""){
+      setFnError(true)
+      isValid = false;
+    } if(user.lastname.trim() == ""){
+      setLnError(true)
+      isValid = false;
+    } else{
+      setFnError(false)
+      setLnError(false)
+    }
+
+    // validate phonenum
+    if(user.phonenum.trim() == "" || !(phoneNumRegex.test(user.phonenum))){
+      setPnError(true)
+      isValid = false;
+    } else{
+      setPnError(false)
+    }
+
+    // validate regnum
+    if(user.regnum.trim() == "" || !(regNumRegex.test(user.regnum))){
+      setRnError(true)
+      isValid = false;
+    } else{
+      setRnError(false)
+    }
+
+    return isValid
+  }
+
+  const submitHandler = (e) => {
+    e.preventDefault()
+    if(validForm()){
+      console.log("Submitted form successfully");
     }
   }
 
@@ -76,32 +116,32 @@ function App() {
   return (
     <Card component="form" variant="outlined" sx={CardStyle}>
       <GlobalStyles/>
-      <Card variant="outlined" sx={{backgroundColor: "#d41d1d", padding: "1em", width: "calc(400px - 2em)"}} hidden={!(error?.universal)} error={error?.universal}>All fields are required</Card>
-      <FormControl variant="outlined" color="success" error={error?.firstname} fullWidth>
+      <Card variant="outlined" sx={{backgroundColor: "#d41d1d", padding: "1em", width: "calc(400px - 2em)"}} hidden={!(uniError)}>All fields are required</Card>
+      <FormControl variant="outlined" color="success" error={fnError} fullWidth>
         <InputLabel htmlFor="firstname">First Name</InputLabel>
         <OutlinedInput id="firstname" label="First Name" ref={inputRefContainer} value={user.firstname} onChange={(e) => setUser({...user,firstname: e.target.value})} aria-describedby="firstname-text"/>
-        <FormHelperText id="firstname-text" hidden={!(error?.firstname)}>First name is required</FormHelperText>
+        <FormHelperText id="firstname-text" hidden={!(fnError)}>First name is required</FormHelperText>
       </FormControl>
-      <FormControl variant="outlined" color="success" error={error?.lastname} fullWidth>
+      <FormControl variant="outlined" color="success" error={lnError} fullWidth>
         <InputLabel htmlFor="lastname">Last Name</InputLabel>
         <OutlinedInput id="lastname" label="Last Name" ref={inputRefContainer} value={user.lastname} onChange={(e) => setUser({...user,lastname: e.target.value})} aria-describedby="lastname-text"/>
-        <FormHelperText id="lastname-text" hidden={!(error?.lastname)}>Last name is required</FormHelperText>
+        <FormHelperText id="lastname-text" hidden={!(lnError)}>Last name is required</FormHelperText>
       </FormControl>
       <FormControl variant="outlined" color="success" fullWidth>
         <InputLabel htmlFor="othername">Other Names</InputLabel>
         <OutlinedInput id="othername" label="Other Names" ref={inputRefContainer} value={user.othername} onChange={(e) => setUser({...user,othername: e.target.value})} aria-describedby="othername-text"/>
       </FormControl>
-      <FormControl variant="outlined"color="success" error={error?.phonenum} fullWidth>
+      <FormControl variant="outlined"color="success" error={pnError} fullWidth>
         <InputLabel htmlFor="phonenum">Phone Number</InputLabel>
         <OutlinedInput id="phonenum" label="Phone Number" ref={inputRefContainer} value={user.phonenum} onChange={(e) => setUser({...user,phonenum: e.target.value})} aria-describedby="phonenum-text"/>
-        <FormHelperText id="phonenum-text" hidden={!(error?.phonenum)}>{phoneNumErrHandler(user.phonenum)}</FormHelperText>
+        <FormHelperText id="phonenum-text" hidden={!(pnError)}>{phoneNumErrHandler(user.phonenum)}</FormHelperText>
       </FormControl>
-      <FormControl variant="outlined" color="success" error={error?.regnum} fullWidth>
+      <FormControl variant="outlined" color="success" error={rnError} fullWidth>
         <InputLabel htmlFor="regnum">Registration Number</InputLabel>
         <OutlinedInput id="regnum" label="Registration Number" ref={inputRefContainer} value={user.regnum} onChange={(e) => setUser({...user,regnum: e.target.value.toUpperCase()})} aria-describedby="regnum-text"/>
-        <FormHelperText id="regnum-text" hidden={!(error?.regnum)}>{regNumErrHandler(user.regnum)}</FormHelperText>
+        <FormHelperText id="regnum-text" hidden={!(rnError)}>{regNumErrHandler(user.regnum)}</FormHelperText>
       </FormControl>
-      <Button variant="contained" color="success" onClick={submitHandler}>Submit</Button>
+      <Button variant="contained" color="success" type="submit" onClick={submitHandler}>Submit</Button>
     </Card>
   )
 }
